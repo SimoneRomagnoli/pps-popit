6935c56da1691635fa434ee608d31ea5623854e3
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-09-13 18:25:27 +0200

    test model, view and gameloop actors

--- src/test/scala/controller/GameLoopTest.scala
+++ src/test/scala/controller/GameLoopTest.scala
@@ -1,5 +1,87 @@
 package controller
 
-object GameLoopTest {
+import akka.actor.testkit.typed.scaladsl.{ScalaTestWithActorTestKit, TestProbe}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+import controller.GameLoop.GameLoopActor
+import controller.GameLoopTest._
+import controller.Messages._
+import org.scalatest.wordspec.AnyWordSpecLike
 
+object GameLoopTest {
+  case class Counter(var value: Double = 0.0) {
+    def inc(amount: Double): Unit = value = value + amount
+  }
+
+  val counter: Counter = Counter()
+  val fastCounter: Counter = Counter()
+
+  val dummyModel: Counter => Behavior[Update] = counter => Behaviors.receiveMessage {
+    case TickUpdate(elapsedTime, replyTo) =>
+      counter.inc(elapsedTime)
+      replyTo ! ModelUpdated(List())
+      Behaviors.same
+    case _ => Behaviors.same
+  }
+
+  def waitSomeTime(): Unit = Thread.sleep(500)
 }
+
+class GameLoopTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+  val model: ActorRef[Update] = testKit.spawn(dummyModel(counter))
+  val fastModel: ActorRef[Update] = testKit.spawn(dummyModel(fastCounter))
+  val view: TestProbe[Render] = testKit.createTestProbe[Render]()
+  val gameLoop: ActorRef[Input] = testKit.spawn(GameLoopActor(model, view.ref))
+  val fastGameLoop: ActorRef[Input] = testKit.spawn(GameLoopActor(fastModel, view.ref))
+
+  "The GameLoop" when {
+    "just created" should {
+      "not update the model" in {
+        counter.value shouldBe 0.0
+        waitSomeTime()
+        counter.value shouldBe 0.0
+      }
+    }
+    "started" should {
+      "update the model" in {
+        counter.value shouldBe 0.0
+        gameLoop ! NewGame()
+        waitSomeTime()
+        counter.value should be > 0.0
+      }
+      "order the view to render" in {
+        view expectMessage RenderEntities(List())
+      }
+    }
+    "stopped" should {
+      "stop updating the model" in {
+        gameLoop ! PauseGame()
+        val lastValue: Double = counter.value
+        lastValue should be > 0.0
+        waitSomeTime()
+        counter.value shouldBe lastValue
+      }
+    }
+    "resumed" should {
+      "resume updating the model" in {
+        val lastValue: Double = counter.value
+        gameLoop ! ResumeGame()
+        waitSomeTime()
+        counter.value should be > lastValue
+      }
+    }
+    "his time ratio gets changed" should {
+      "update the model according to the new time ratio" in {
+        fastGameLoop ! NewGame()
+        fastGameLoop ! NewTimeRatio(2.0)
+
+        val initialValue: (Double, Double) = (counter.value, fastCounter.value)
+        waitSomeTime()
+        val finalValue: (Double, Double) = (counter.value, fastCounter.value)
+
+        (finalValue._1 - initialValue._1) should be < (finalValue._2 - initialValue._2)
+      }
+    }
+  }
+}
+

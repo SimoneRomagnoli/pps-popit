e16639959b8232e970a4d9b475a0371fbe56963a
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-09-17 12:50:58 +0200

    add rendering map message

--- src/main/scala/controller/Messages.scala
+++ src/main/scala/controller/Messages.scala
@@ -1,6 +1,8 @@
 package controller
 
 import akka.actor.typed.ActorRef
+import model.maps.Grids.Grid
+import model.maps.Tracks.Track
 
 object Messages {
   //TRAITS
@@ -11,12 +13,14 @@ object Messages {
 
   //VIEW
   case class RenderEntities(entities: List[Any]) extends Render
+  case class RenderMap(grid: Grid, track:Track) extends Render
 
   //CONTROLLER
   case class NewGame() extends Input
   case class PauseGame() extends Input
   case class ResumeGame() extends Input
   case class NewTimeRatio(value: Double) extends Input
+  case class NewMap() extends Input
 
   //GAME LOOP
   case object Tick extends Input

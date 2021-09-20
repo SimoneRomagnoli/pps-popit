3203ab7652a84deb088fd0a1154d1a3a0cfaf1de
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-09-20 17:57:02 +0200

    remove unused methods

--- src/main/scala/model/maps/Tracks.scala
+++ src/main/scala/model/maps/Tracks.scala
@@ -38,21 +38,6 @@ object Tracks {
     case object DOWN extends Direction
     case object LEFT extends Direction
     case object RIGHT extends Direction
-
-    /**
-     * Given the ordered first and second direction, it determines if they form a left or right turn
-     *
-     * @param first [[Direction]]
-     * @param second [[Direction]]
-     * @return [[LEFT]], [[RIGHT]] or [[NONE]]
-     */
-    def turnBetween(first: Direction, second: Direction): Direction = first match {
-      case UP if second == RIGHT || second == LEFT => second
-      case DOWN if second == RIGHT || second == LEFT => second.opposite
-      case LEFT if second == UP || second == DOWN => second.turnRight
-      case RIGHT if second == UP || second == DOWN => second.turnLeft
-      case _ => NONE
-    }
   }
 
   /**

f50a03e5bfa2d3575705230f2ff84b684445b1cf
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-09-21 21:00:26 +0200

    add balloon on road test

--- src/test/scala/model/maps/MapsRenderingTest.scala
+++ src/test/scala/model/maps/MapsRenderingTest.scala
@@ -9,26 +9,43 @@ import scalafx.scene.Scene
 import scalafx.scene.canvas.Canvas
 import scalafx.scene.layout.Pane
 import MapsRenderingTest._
-import akka.actor.typed.{ ActorRef, ActorSystem }
+import akka.actor.typed.{ ActorRef, ActorSystem, Behavior }
 import akka.actor.typed.scaladsl.Behaviors
-import controller.Messages.{ Message, Render, RenderEntities, RenderMap }
+import controller.GameLoop.GameLoopActor
+import controller.Messages.{
+  EntityUpdated,
+  Input,
+  Message,
+  ModelUpdated,
+  Render,
+  RenderMap,
+  Start,
+  TickUpdate,
+  Update,
+  UpdateEntity
+}
+import model.Positions._
 import javafx.scene.paint.ImagePattern
 import model.maps.Tracks.Track
 import org.scalatest.Ignore
 import javafx.scene.image.Image
+import model.entities.Entities.Entity
 import scalafx.scene.paint.Color
-import view.View.ViewActor
+import view.View.{ Board, ViewActor }
 import model.entities.balloons.BalloonType._
+import model.entities.balloons.Balloons.Balloon
+import model.maps.Cells.Cell
+import model.maps.Tracks.Directions.RIGHT
+import utils.Constants
 
 import scala.language.postfixOps
-
 import java.io.File
 
 object MapsRenderingTest {
   val cellSize: Int = 60
   val canvas: Canvas = new Canvas(1200, 600)
 
-  val appBuilder: Canvas => JFXApp = canvas =>
+  val canvasAppBuilder: Canvas => JFXApp = canvas =>
     new JFXApp {
 
       stage = new PrimaryStage {
@@ -41,6 +58,70 @@ object MapsRenderingTest {
             children = Seq(canvas)
           }
         }
+      }
+    }
+
+  val paneAppBuilder: Board => JFXApp = board =>
+    new JFXApp {
+
+      stage = new PrimaryStage {
+        title = "Test"
+        resizable = false
+
+        scene = new Scene(Constants.width, Constants.height) {
+          root = board
+        }
+      }
+    }
+
+  val dummyModel: (List[Entity], Track) => Behavior[Update] = (entities, track) =>
+    Behaviors.setup { ctx =>
+      var entitiesUpdated: List[Entity] = List()
+      var gameLoop: ActorRef[Input] = null
+      val actors: List[ActorRef[Update]] =
+        entities map (e => ctx.spawnAnonymous(dummyBalloonActor(e.asInstanceOf[Balloon])))
+      Behaviors.receiveMessage {
+        case TickUpdate(elapsedTime, replyTo) =>
+          gameLoop = replyTo
+          actors foreach {
+            _ ! UpdateEntity(elapsedTime, entities, ctx.self, track)
+          }
+          Behaviors.same
+
+        case EntityUpdated(entity) =>
+          entitiesUpdated = entitiesUpdated appended entity
+          if (entitiesUpdated.size == entities.size) {
+            gameLoop ! ModelUpdated(entitiesUpdated)
+            dummyModel(entitiesUpdated, track)
+          } else {
+            Behaviors.same
+          }
+
+        case _ => Behaviors.same
+      }
+    }
+
+  var linearPosition: Double = 0.0
+
+  val dummyBalloonActor: Balloon => Behavior[Update] = balloon =>
+    Behaviors.setup { _ =>
+      Behaviors.receiveMessage { case UpdateEntity(elapsedTime, _, replyTo, track) =>
+        linearPosition += 1.5 * elapsedTime
+        val currentCell: Cell = track.cells(linearPosition.toInt)
+        val newBalloon: Balloon = currentCell match {
+          case start if start == track.start =>
+            balloon.in(
+              currentCell.exactPosition(RIGHT)(linearPosition - linearPosition.toInt) - (15.0, 20.0)
+            )
+          case _ =>
+            balloon.in(
+              currentCell.exactPosition(track.cells(linearPosition.toInt - 1).direction)(
+                linearPosition - linearPosition.toInt
+              ) - (15.0, 20.0)
+            )
+        }
+        replyTo ! EntityUpdated(newBalloon)
+        dummyBalloonActor(newBalloon)
       }
     }
 }
@@ -60,7 +141,7 @@ class MapsRenderingTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
             cellSize - 0.5
           )
         }
-        appBuilder(canvas).main(Array())
+        canvasAppBuilder(canvas).main(Array())
       }
       "design grass" in {
         val img: File = new File("res/grass-pattern.png")
@@ -73,7 +154,7 @@ class MapsRenderingTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
             cellSize - 0.5
           )
         }
-        appBuilder(canvas).main(Array())
+        canvasAppBuilder(canvas).main(Array())
       }
     }
     "testing tracks" should {
@@ -93,32 +174,36 @@ class MapsRenderingTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
             cell._1.y * cellSize + 30
           )
         }
-        appBuilder(canvas).main(Array())
+        canvasAppBuilder(canvas).main(Array())
       }
       "design the road" in {
         ActorSystem[Message](
           Behaviors.setup[Message] { ctx =>
             val view: ActorRef[Render] = ctx.spawn(ViewActor(), "view")
-            val grid: Grid = Grid(16, 8)
+            val grid: Grid = Grid(Constants.widthRatio, Constants.heightRatio)
             view ! RenderMap(grid, Track(grid))
-            view ! RenderEntities(List((Red balloon) in (300.0, 300.0)))
             Behaviors.empty
           },
           "system"
         )
-        appBuilder(ViewActor.canvas).main(Array())
+        paneAppBuilder(ViewActor.board).main(Array())
       }
       "design balloons on the road" in {
         ActorSystem[Message](
           Behaviors.setup[Message] { ctx =>
             val view: ActorRef[Render] = ctx.spawn(ViewActor(), "view")
-            val grid: Grid = Grid(16, 8)
-            view ! RenderMap(grid, Track(grid))
+            val grid: Grid = Grid(Constants.widthRatio, Constants.heightRatio)
+            val track: Track = Track(grid)
+            view ! RenderMap(grid, track)
+            val entities: List[Entity] = List((Red balloon) in track.start.topLeftPosition)
+            val model: ActorRef[Update] = ctx.spawn(dummyModel(entities, track), "model")
+            val gameLoop: ActorRef[Input] = ctx.spawn(GameLoopActor(model, view), "gameLoop")
+            gameLoop ! Start()
             Behaviors.empty
           },
           "system"
         )
-        appBuilder(ViewActor.canvas).main(Array())
+        paneAppBuilder(ViewActor.board).main(Array())
       }
     }
   }

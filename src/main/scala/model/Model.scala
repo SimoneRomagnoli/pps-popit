5450bc90f63e3ff32ac09e9b5aaddf4ce6fbbe31
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-09-21 19:54:30 +0200

    new setup with map initialization

--- src/main/scala/model/Model.scala
+++ src/main/scala/model/Model.scala
@@ -3,36 +3,48 @@ package model
 import akka.actor.typed.{ ActorRef, Behavior }
 import akka.actor.typed.scaladsl.{ ActorContext, Behaviors }
 import controller.Messages
-import controller.Messages.{ EntityUpdated, TickUpdate, Update, UpdateEntity }
+import controller.Messages.{ EntityUpdated, MapCreated, NewMap, TickUpdate, Update, UpdateEntity }
 import model.entities.Entities.Entity
+import model.entities.balloons.BalloonType.Red
+import model.maps.Grids.Grid
+import model.maps.Tracks.Track
 
 object Model {
 
   object ModelActor {
 
-    def apply(): Behavior[Update] = Behaviors.setup { ctx =>
-      running(ctx, List(), Seq())
+    def apply(): Behavior[Update] = Behaviors setup init
+
+    def init(ctx: ActorContext[Update]): Behavior[Update] = Behaviors.receiveMessage {
+      case NewMap(replyTo) =>
+        val grid: Grid = Grid(16, 8)
+        val track: Track = Track(grid)
+        replyTo ! MapCreated(grid, track)
+        val entities: List[Entity] = List((Red balloon) in track.start.topLeftPosition)
+        running(ctx, entities, Seq(), track)
     }
 
     def running(
         ctx: ActorContext[Update],
         entities: List[Entity],
-        actors: Seq[ActorRef[Update]]): Behavior[Update] =
+        actors: Seq[ActorRef[Update]],
+        track: Track): Behavior[Update] =
       Behaviors.receiveMessage { case TickUpdate(elapsedTime, replyTo) =>
         actors foreach {
-          _ ! UpdateEntity(elapsedTime, entities, ctx.self)
+          _ ! UpdateEntity(elapsedTime, entities, ctx.self, track)
         }
-        updating(ctx, entities, actors, replyTo)
+        updating(ctx, entities, actors, replyTo, track)
       }
 
     def updating(
         ctx: ActorContext[Update],
         entities: List[Entity],
         actors: Seq[ActorRef[Update]],
-        replyTo: ActorRef[Messages.Input]): Behavior[Update] =
+        replyTo: ActorRef[Messages.Input],
+        track: Track): Behavior[Update] =
       Behaviors.receiveMessage {
         case EntityUpdated(_) =>
-          running(ctx, entities, actors)
+          running(ctx, entities, actors, track)
         case _ => Behaviors.same
       }
   }

d778f0f5adb88c83ba337c26e75638d4d0278924
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-09-22 17:22:59 +0200

    insert track that turns back

--- src/main/resources/theories/tracks.pl
+++ src/main/resources/theories/tracks.pl
@@ -14,6 +14,7 @@ notEnoughConsecutive(Cons, Max):- Delta is 100 / Max, Prob is Cons * Delta, lowe
 % All path
 allPath(c(X0, Y0), c(XF, YF), P):-allPath(c(X0, Y0), c(XF, YF), P, [], 1, 0).
 allPath(c(X,Y), c(X,Y), [c(X,Y)|[]], Acc, _, _).
-allPath(c(X0, Y0), c(XF, YF), [c(X0,Y0)|P], Acc, ConsX, ConsY):- enoughConsecutive(ConsY, ConsY2, 5), Y1 is Y0+1, Y1=<7, suitable(X0, Y1, Acc), allPath(c(X0,Y1), c(XF,YF), P, [c(X0,Y0)|Acc], 0, ConsY2).
+allPath(c(X0, Y0), c(XF, YF), [c(X0,Y0)|P], Acc, ConsX, ConsY):- ConsY > 3, ConsX=<0, enoughConsecutive(ConsX, _, 3), ConsX2 is ConsX-1, ConsY2 is ConsY-1, X1 is X0-1, X1>=0, suitable(X1, Y0, Acc), allPath(c(X1,Y0), c(XF,YF), P, [c(X0,Y0)|Acc], ConsX2, ConsY2).
+allPath(c(X0, Y0), c(XF, YF), [c(X0,Y0)|P], Acc, ConsX, ConsY):- enoughConsecutive(ConsY, ConsY2, 5), Y1 is Y0+1, Y1=<8, suitable(X0, Y1, Acc), allPath(c(X0,Y1), c(XF,YF), P, [c(X0,Y0)|Acc], 0, ConsY2).
 allPath(c(X0, Y0), c(XF, YF), [c(X0,Y0)|P], Acc, ConsX, ConsY):- enoughConsecutive(ConsY, ConsY2, 5), Y1 is Y0-1, Y1>=0, suitable(X0, Y1, Acc), allPath(c(X0,Y1), c(XF,YF), P, [c(X0,Y0)|Acc], 0, ConsY2).
-allPath(c(X0, Y0), c(XF, YF), [c(X0,Y0)|P], Acc, ConsX, ConsY):- ConsY2 is ConsY-1, X1 is X0+1, X1=<XF, suitable(X1, Y0, Acc), allPath(c(X1,Y0), c(XF,YF), P, [c(X0,Y0)|Acc], ConsX2, ConsY2).
+allPath(c(X0, Y0), c(XF, YF), [c(X0,Y0)|P], Acc, ConsX, ConsY):- ConsX2 is ConsX+1, ConsY2 is ConsY-1, X1 is X0+1, X1=<XF, suitable(X1, Y0, Acc), allPath(c(X1,Y0), c(XF,YF), P, [c(X0,Y0)|Acc], ConsX2, ConsY2).

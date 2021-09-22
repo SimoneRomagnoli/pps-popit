c6f4fdf2c20027cd4fb7670fcac5b598ae3dd36d
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-09-22 17:23:44 +0200

    use default width if screen is not available (mainly on github actions)

--- src/main/scala/utils/Constants.scala
+++ src/main/scala/utils/Constants.scala
@@ -1,11 +1,13 @@
 package utils
 
-import java.awt.{ Dimension, Toolkit }
+import java.awt.{ GraphicsEnvironment, Toolkit }
 
 object Constants {
-  val screen: Dimension = Toolkit.getDefaultToolkit.getScreenSize
+
+  val screenWidth: Double =
+    if (GraphicsEnvironment.isHeadless) 800.0 else Toolkit.getDefaultToolkit.getScreenSize.getWidth
   val widthRatio: Int = 16
   val heightRatio: Int = 9
-  val width: Double = screen.getWidth * 3 / 4
+  val width: Double = screenWidth * 3 / 4
   val height: Double = width * heightRatio / widthRatio
 }

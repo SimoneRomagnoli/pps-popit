bb60d21ad8355124099c8a5ffbd1e75f8c1eed8c
Tommaso Mandoloni <tommaso.mandoloni@studio.unibo.it>
2021-09-23 08:59:01 +0200

    Tests for tower actor

---
+++ src/test/scala/model/actors/TowerActorTest.scala
@@ -0,0 +1,99 @@
+package model.actors
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.typed.{ ActorRef, Behavior }
+import akka.actor.typed.scaladsl.Behaviors
+import controller.Messages.{
+  BalloonDetected,
+  BalloonMoved,
+  SearchBalloon,
+  Tick,
+  Update,
+  UpdatePosition
+}
+import model.Positions.Vector2D
+import model.actors.TowerActorTest.{
+  balloonDetected,
+  balloonPosition,
+  dummyBalloonActor,
+  dummyModel,
+  towerPosition,
+  waitSomeTime
+}
+import model.entities.balloons.Balloons.{ Balloon, Simple }
+import model.entities.towers.Towers.BasicTower
+import org.scalatest.wordspec.AnyWordSpecLike
+import utils.Constants.{ shotRatio, sightRange }
+
+object TowerActorTest {
+
+  val balloonPosition: Vector2D = (60.0, 80.0)
+  val towerPosition: Vector2D = (20.0, 10.0)
+  var balloonDetected: Boolean = false
+
+  val dummyBalloonActor: Balloon => Behavior[Update] = b =>
+    Behaviors.receiveMessage {
+      case UpdatePosition(replyTo) =>
+        val newBalloon: Balloon = b in ((b.position.x - 20, b.position.y - 30))
+        replyTo ! BalloonMoved(newBalloon)
+        dummyBalloonActor(newBalloon)
+      case _ => Behaviors.same
+    }
+
+  val dummyModel: ActorRef[Update] => Behavior[Update] = t =>
+    Behaviors.setup { ctx =>
+      Behaviors.receiveMessage {
+        case Tick(replyTo) =>
+          balloonDetected = false
+          replyTo ! UpdatePosition(ctx.self)
+          Behaviors.same
+        case BalloonMoved(entity) =>
+          t ! SearchBalloon(ctx.self, entity)
+          Behaviors.same
+        case BalloonDetected() =>
+          balloonDetected = true
+          Behaviors.same
+        case _ => Behaviors.same
+      }
+    }
+
+  def waitSomeTime(): Unit = Thread.sleep(500)
+}
+
+class TowerActorTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+
+  val towerActor: ActorRef[Update] =
+    testKit.spawn(TowerActor(new BasicTower(towerPosition, sightRange, shotRatio)))
+  val model: ActorRef[Update] = testKit.spawn(dummyModel(towerActor))
+
+  val balloonActor: ActorRef[Update] =
+    testKit.spawn(dummyBalloonActor(Simple(balloonPosition)))
+
+  "The tower actor" when {
+    "has just spawned, it" should {
+      "not see the balloon" in {
+        model ! Tick(balloonActor)
+        waitSomeTime()
+        balloonDetected shouldBe false
+      }
+    }
+    "the balloon moves through the map, it" should {
+      "not see the balloon immediately, but after the second move" in {
+        model ! Tick(balloonActor)
+        waitSomeTime()
+        model ! Tick(balloonActor)
+        waitSomeTime()
+        balloonDetected shouldBe true
+      }
+    }
+    "the balloon goes far away, it" should {
+      "not see the balloon any more" in {
+        model ! Tick(balloonActor)
+        waitSomeTime()
+        model ! Tick(balloonActor)
+        waitSomeTime()
+        balloonDetected shouldBe false
+      }
+    }
+  }
+}
--- src/test/scala/model/entities/towers/TowersTest.scala
+++ src/test/scala/model/entities/towers/TowersTest.scala
@@ -1,19 +1,12 @@
 package model.entities.towers
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ ActorRef, Behavior }
-import controller.Messages._
 import model.Positions.Vector2D
-import model.actors.TowerActor
 import model.entities.balloons.Balloons.{ Balloon, Simple }
 import model.entities.towers.Towers.{ BasicTower, Tower }
 import model.entities.towers.TowersTest.{
   balloon,
-  balloonDetected,
   balloonPosition,
-  dummyBalloonActor,
-  dummyModel,
   lastShotTime,
   tower,
   towerPosition,
@@ -28,51 +21,17 @@ object TowersTest {
 
   val balloonPosition: Vector2D = (60.0, 80.0)
   val towerPosition: Vector2D = (20.0, 10.0)
-  var balloonDetected: Boolean = false
   var lastShotTime: Double = 0.0
 
   val tower: Tower = new BasicTower(towerPosition, sightRange, shotRatio)
   var balloon: Balloon = Simple(balloonPosition)
-
-  val dummyBalloonActor: Balloon => Behavior[Update] = b =>
-    Behaviors.receiveMessage {
-      case UpdatePosition(replyTo) =>
-        val newBalloon: Balloon = b in ((b.position.x - 20, b.position.y - 30))
-        replyTo ! BalloonMoved(newBalloon)
-        dummyBalloonActor(newBalloon)
-      case _ => Behaviors.same
-    }
-
-  val dummyModel: ActorRef[Update] => Behavior[Update] = t =>
-    Behaviors.setup { ctx =>
-      Behaviors.receiveMessage {
-        case Tick(replyTo) =>
-          balloonDetected = false
-          replyTo ! UpdatePosition(ctx.self)
-          Behaviors.same
-        case BalloonMoved(entity) =>
-          t ! SearchBalloon(ctx.self, entity)
-          Behaviors.same
-        case BalloonDetected() =>
-          balloonDetected = true
-          Behaviors.same
-        case _ => Behaviors.same
-      }
-    }
 
   def waitSomeTime(): Unit = Thread.sleep(500)
 }
 
 class TowersTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
 
-  val towerActor: ActorRef[Update] =
-    testKit.spawn(TowerActor(new BasicTower(towerPosition, sightRange, shotRatio)))
-  val model: ActorRef[Update] = testKit.spawn(dummyModel(towerActor))
-
-  val balloonActor: ActorRef[Update] =
-    testKit.spawn(dummyBalloonActor(Simple(balloonPosition)))
-
-  "During a simple running" when {
+  "During a simple game" when {
     "tower and balloon has just spawned, the tower" should {
       "not see the balloon because it is across the map" in {
         (tower position) shouldBe towerPosition
@@ -110,34 +69,6 @@ class TowersTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
         newTower canAttackAfter lastShotTime shouldBe false
         waitSomeTime()
         newTower canAttackAfter lastShotTime shouldBe true
-      }
-    }
-  }
-
-  "During a simple running with actors" when {
-    "actors has just spawned, the tower" should {
-      "not see the balloon" in {
-        model ! Tick(balloonActor)
-        waitSomeTime()
-        balloonDetected shouldBe false
-      }
-    }
-    "the balloon moves through the map, the tower" should {
-      "not see the balloon immediately, but after the second move" in {
-        model ! Tick(balloonActor)
-        waitSomeTime()
-        model ! Tick(balloonActor)
-        waitSomeTime()
-        balloonDetected shouldBe true
-      }
-    }
-    "the balloon goes far away, the tower" should {
-      "not see the balloon any more" in {
-        model ! Tick(balloonActor)
-        waitSomeTime()
-        model ! Tick(balloonActor)
-        waitSomeTime()
-        balloonDetected shouldBe false
       }
     }
   }

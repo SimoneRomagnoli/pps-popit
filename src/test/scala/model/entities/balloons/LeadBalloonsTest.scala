636cbd68ae30c10bbfb30869d67329de7a32cd52
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-10-05 18:03:29 +0200

    Check position and movement of lead balloons

--- src/test/scala/model/entities/balloons/LeadBalloonsTest.scala
+++ src/test/scala/model/entities/balloons/LeadBalloonsTest.scala
@@ -1,7 +1,7 @@
 package model.entities.balloons
 
 import model.Positions.Vector2D
-import model.entities.balloons.BalloonLives.Green
+import model.entities.balloons.BalloonLives.{ Green, Red }
 import model.entities.balloons.balloontypes.LeadBalloons.lead
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -16,6 +16,18 @@ class LeadBalloonsTest extends AnyFlatSpec with Matchers {
     lead(Green balloon).position shouldBe zeroVector
     lead(Green balloon).speed shouldBe oneVector
     lead((Green balloon) in oneVector).position shouldBe oneVector
+  }
+
+  it should "be able to change speed and position" in {
+    (lead(Green balloon) in oneVector).position shouldBe oneVector
+    (lead(Green balloon) in oneVector).speed shouldBe oneVector
+    (lead(Green balloon) at oneVector).speed shouldBe oneVector
+    (lead(Green balloon) at oneVector).position shouldBe zeroVector
+  }
+
+  it should "be able to move" in {
+    (lead(Red balloon) at zeroVector).update(5.0).position should not be zeroVector
+    (lead(Green balloon) at zeroVector).update(5.0).position should not be zeroVector
   }
 
 }

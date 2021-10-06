e0e92b05814e023903fd00a6880a3f641a1e2f96
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-10-07 01:23:13 +0200

    adapt controller to companion object pattern

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -1,19 +1,9 @@
 package controller
 
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{ ActorContext, Behaviors }
 import akka.actor.typed.{ ActorRef, Behavior }
 import controller.GameLoop.GameLoopActor
-import controller.Messages.{
-  EntitySpawned,
-  Input,
-  NewGame,
-  PauseGame,
-  PlaceTower,
-  Render,
-  ResumeGame,
-  Start,
-  Update
-}
+import controller.Messages._
 import model.Model.ModelActor
 import model.actors.TowerActor
 import model.entities.bullets.Bullets.Bullet
@@ -26,27 +16,46 @@ object Controller {
   object ControllerActor {
 
     def apply(view: ActorRef[Render]): Behavior[Input] = Behaviors.setup { ctx =>
-      val model: ActorRef[Update] = ctx.spawn(ModelActor(), "model")
-      var gameLoops: Seq[ActorRef[Input]] = Seq()
-      val gameLoop: () => ActorRef[Input] = () => gameLoops.head
-      Behaviors.receiveMessage {
-        case NewGame() =>
-          val actor: ActorRef[Input] = ctx.spawn(GameLoopActor(model, view), "gameLoop")
-          gameLoops = gameLoops :+ actor
-          gameLoop() ! Start()
-          Behaviors.same
+      ControllerActor(ctx, view, ctx.spawn(ModelActor(ctx.self), "model")).default()
+    }
+  }
 
-        case PlaceTower(cell, towerType) =>
-          val tower: Tower[Bullet] = towerType.tower in cell
-          model ! EntitySpawned(tower, ctx.spawnAnonymous(TowerActor(tower)))
-          Behaviors.same
+  case class ControllerActor private (
+      ctx: ActorContext[Input],
+      view: ActorRef[Render],
+      var model: ActorRef[Update],
+      var gameLoops: Seq[ActorRef[Input]] = Seq()) {
+    private def gameLoop: () => ActorRef[Input] = () => gameLoops.head
 
-        case input: Input if input.isInstanceOf[PauseGame] || input.isInstanceOf[ResumeGame] =>
-          gameLoop() ! input
-          Behaviors.same
+    def default(): Behavior[Input] = Behaviors.receiveMessage {
+      case NewGame() =>
+        val actor: ActorRef[Input] = ctx.spawn(GameLoopActor(model, view), "gameLoop")
+        gameLoops = gameLoops :+ actor
+        gameLoop() ! Start()
+        Behaviors.same
 
-        case _ => Behaviors.same
-      }
+      case MvcInteraction(replyTo, message) =>
+        model ! message.asInstanceOf[Update]
+        interacting(replyTo)
+
+      case PlaceTower(cell, towerType) =>
+        val tower: Tower[Bullet] = towerType.tower in cell
+        model ! EntitySpawned(tower, ctx.spawnAnonymous(TowerActor(tower)))
+        Behaviors.same
+
+      case input: Input if input.isInstanceOf[PauseGame] || input.isInstanceOf[ResumeGame] =>
+        gameLoop() ! input
+        Behaviors.same
+
+      case _ => Behaviors.same
+    }
+
+    def interacting(replyTo: ActorRef[Message]): Behavior[Input] = Behaviors.receiveMessage {
+      case TowerOption(tower) =>
+        replyTo ! TowerOption(tower)
+        default()
+
+      case _ => Behaviors.same
     }
   }
 }

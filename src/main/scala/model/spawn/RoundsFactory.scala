c3482131b69cbb804b5904f88318438e3934c1b0
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-10-16 11:21:53 +0200

    Add round factory: still to implement spawn logic

--- src/main/scala/model/spawn/RoundsFactory.scala
+++ src/main/scala/model/spawn/RoundsFactory.scala
@@ -1,9 +1,12 @@
 package model.spawn
 
 import cats.effect.IO
+import model.entities.balloons.BalloonDecorations.Regenerating
 import model.entities.balloons.BalloonLives.{ Blue, Green, Red }
 import model.spawn.RoundBuilders.{ add, RichIO }
 import model.spawn.Rounds.{ Round, Streak }
+
+import scala.util.Random
 
 object RoundsFactory {
   private var round: Int = 0
@@ -12,10 +15,14 @@ object RoundsFactory {
 
   def nextRound(): Round = (for {
     _ <- incrementRound()
-    _ <- add(Streak(round * 10) :- Red)
-    _ <- add(Streak(round * 10) :- Blue)
-    _ <- add(Streak(round * 5) :- Green)
+    _ <- add(Streak(nBalloons(1)) :- Red)
+    _ <- add(Streak(nBalloons(0.6)) :- Blue)
+    _ <- add(Streak(nBalloons(0.4)) :- Green)
+    _ <- if (round > 0) add(Streak(nBalloons(0.8)) :- (Red & Regenerating)) else IO.unit
   } yield ()).get
 
   private def incrementRound(): IO[Unit] = IO(round += 1)
+
+  private val nBalloons: Double => Int = probability =>
+    (Random.nextDouble() * round * probability * 10).toInt
 }

0ae910a2f331798c16fef0586551fa39c8bc8fe3
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-10-19 07:51:19 +0200

    Introduce functions for balloons quantity in rounds

--- src/main/scala/model/spawn/RoundsFactory.scala
+++ src/main/scala/model/spawn/RoundsFactory.scala
@@ -2,17 +2,15 @@ package model.spawn
 
 import cats.effect.IO
 import model.entities.balloons.BalloonDecorations.Regenerating
-import model.entities.balloons.BalloonLives.{ Blue, Green, Red }
-import model.spawn.RoundBuilders.{ add, RichIO }
-import model.spawn.Rounds.{ Round, Streak }
+import model.entities.balloons.BalloonLives.{Blue, Green, Red}
+import model.spawn.BalloonsFunctions._
+import model.spawn.RoundBuilders.{RichIO, add}
+import model.spawn.Rounds.{Round, Streak}
 
 import scala.concurrent.duration.DurationInt
 
 object RoundsFactory {
-  private var round: Int = 14
-
-  private val easyRoundLimit: Int = 15
-  private val regeneratingRoundLimit: Int = 20
+  private var round: Int = 0
 
   def currentRound: Int = round
 
@@ -27,26 +25,39 @@ object RoundsFactory {
   }
 
   private def easyRounds(round: Int): IO[Unit] = for {
-    _ <- add(
-      (Streak(
-        if (round <= 7) 5 * round
-        else 5 * (easyRoundLimit - round)
-      ) :- Red) @@ (1000 / (round % 5 + 1)).milliseconds
-    )
-    _ <-
-      if (round >= 5) add((Streak(round) :- Blue) @@ (1000 / (round % 5 + 1)).milliseconds)
-      else IO.unit
-    _ <-
-      if (round >= 10) add((Streak(round / 2) :- Green) @@ (1000 / (round % 5 + 1)).milliseconds)
-      else IO.unit
+    _ <- add((Streak(redBalloonQuantity(round)) :- Red) @@ balloonsDelay(round).milliseconds)
+    _ <- add((Streak(blueBalloonQuantity(round)) :- Blue) @@ balloonsDelay(round).milliseconds)
+    _ <- add((Streak(greenBalloonQuantity(round)) :- Green) @@ balloonsDelay(round).milliseconds)
   } yield ()
 
   private def regeneratingRounds(round: Int): IO[Unit] = for {
     _ <- easyRounds(round * 3)
-    _ <- add((Streak(round * 3) :- (Red & Regenerating)) @@ 300.milliseconds)
-    _ <- add((Streak(round * 2) :- (Blue & Regenerating)) @@ 300.milliseconds)
-    _ <- add((Streak(round) :- (Green & Regenerating)) @@ 300.milliseconds)
+    _ <- add(Streak(round * 3) :- (Red & Regenerating))
+    _ <- add(Streak(round * 2) :- (Blue & Regenerating))
+    _ <- add(Streak(round) :- (Green & Regenerating))
   } yield ()
 
   private def incrementRound(): IO[Unit] = IO(round += 1)
+}
+
+object BalloonsFunctions {
+  val easyRoundLimit: Int = 15
+  val regeneratingRoundLimit: Int = 20
+
+  val balloonsDelay: Int => Int = x => 1000 / (x % 5 + 1)
+
+  val redBalloonQuantity: Int => Int = {
+    case x if x < easyRoundLimit / 2 => (easyRoundLimit / 3) * x
+    case x => (easyRoundLimit / 3) * (easyRoundLimit - x)
+  }
+
+  val blueBalloonQuantity: Int => Int = {
+    case x if x >= (easyRoundLimit / 3) => x
+    case _ => 0
+  }
+
+  val greenBalloonQuantity: Int => Int = {
+    case x if x >= (easyRoundLimit / 3 * 2) => x / 2
+    case _ => 0
+  }
 }

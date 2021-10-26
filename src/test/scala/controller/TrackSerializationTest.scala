512089ae79961210a9b1dbc3917248c254190104
Tommaso Mandoloni <tommaso.mandoloni@studio.unibo.it>
2021-10-26 10:25:14 +0200

    Fix errors due to missing json directory

--- src/test/scala/controller/TrackSerializationTest.scala
+++ src/test/scala/controller/TrackSerializationTest.scala
@@ -4,9 +4,6 @@ import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import alice.tuprolog.Term
 import controller.TrackSerializationTest.{ engine, listSize, query }
 import controller.files.FileCoder
-import controller.files.FileCoders.{ trackDecoder, trackEncoder }
-import io.circe.Json
-import io.circe.syntax.EncoderOps
 import model.maps.Grids.Grid
 import model.maps.Tracks.Directions.{ LEFT, RIGHT }
 import model.maps.Tracks.Track
@@ -41,17 +38,6 @@ class TrackSerializationTest extends ScalaTestWithActorTestKit with AnyWordSpecL
   val emptyList: List[Track] = List()
 
   "The controller" when {
-    "it has to save a list of tracks on file" should {
-      "serialize and deserialize the entire list to save and retrieve it" in {
-        val serialized: Json = trackList.asJson
-
-        val deserialized: List[Track] = serialized.as[List[Track]].getOrElse(List())
-
-        for (i <- trackList.indices)
-          trackList(i).equals(deserialized(i)) shouldBe true
-      }
-    }
-
     "use a FileCoder" should {
       "be able to serialize and deserialize the track list" in {
         coder.serialize(trackList)

f885dc55914f034d6f40acce2786213d87a705d0
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-10-29 10:39:51 +0200

    Move time settings to settings object

--- src/main/scala/controller/interaction/GameLoop.scala
+++ src/main/scala/controller/interaction/GameLoop.scala
@@ -1,16 +1,16 @@
 package controller.interaction
 
-import akka.actor.typed.scaladsl.{ ActorContext, Behaviors }
-import akka.actor.typed.{ ActorRef, Behavior }
-import controller.Controller.ControllerMessages.{ PauseGame, ResumeGame, StartAnimation }
-import Messages.{ Input, Render, Update }
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.actor.typed.{ActorRef, Behavior}
+import controller.Controller.ControllerMessages.{PauseGame, ResumeGame, StartAnimation}
+import Messages.{Input, Render, Update}
 import controller.interaction.GameLoop.GameLoopMessages._
-import controller.interaction.GameLoop.Time._
+import controller.settings.Settings.Time.{TimeSettings, delay, elapsedTime, frameRate, timeRatio}
 import model.Model.ModelMessages.TickUpdate
 import model.entities.Entities.Entity
 import model.maps.Tracks.Track
 import model.stats.Stats.GameStats
-import view.View.ViewMessages.{ RenderEntities, RenderGameOver, RenderStats }
+import view.View.ViewMessages.{RenderEntities, RenderGameOver, RenderStats}
 
 import scala.concurrent.duration.DurationDouble
 
@@ -37,13 +37,13 @@ object GameLoop {
    */
   object GameLoopActor {
 
-    def apply(model: ActorRef[Update], view: ActorRef[Render]): Behavior[Input] = Behaviors.setup {
+    def apply(model: ActorRef[Update], view: ActorRef[Render], timeSettings: TimeSettings): Behavior[Input] = Behaviors.setup {
       ctx =>
         Behaviors.receiveMessage {
           case Start() =>
             Behaviors.withTimers { timers =>
               timers.startTimerWithFixedDelay(Tick, delay(frameRate).seconds)
-              GameLoopActor(ctx, model, view).running()
+              GameLoopActor(ctx, model, view, timeSettings).running()
             }
           case _ => Behaviors.same
         }
@@ -60,11 +60,11 @@ object GameLoop {
       ctx: ActorContext[Input],
       model: ActorRef[Update],
       view: ActorRef[Render],
-      var timeRatio: Double = 1.0) {
+      timeSettings: TimeSettings) {
 
     private def running(): Behavior[Input] = Behaviors.receiveMessage {
       case Tick =>
-        model ! TickUpdate(elapsedTime(frameRate)(timeRatio), ctx.self)
+        model ! TickUpdate(elapsedTime(timeSettings.frameRate)(timeSettings.timeRatio), ctx.self)
         Behaviors.same
 
       case ModelUpdated(entities, animations) =>
@@ -104,14 +104,5 @@ object GameLoop {
         running()
       case _ => Behaviors.same
     }
-  }
-
-  object Time {
-    val frameRate: Double = 60.0
-    val truncate: Double => Double = n => (n * 1000).round / 1000.toDouble
-    val delay: Double => Double = n => truncate(1.0 / n)
-
-    def elapsedTime(frameRate: Double)(implicit timeRatio: Double = 1.0): Double =
-      delay(frameRate) * timeRatio
   }
 }
--- src/main/scala/controller/settings/Settings.scala
+++ src/main/scala/controller/settings/Settings.scala
@@ -1,5 +1,7 @@
 package controller.settings
 
+import controller.settings.Settings.Time.TimeSettings
+
 /**
  * Wraps all settings that can be set from the homonym menu.
  */
@@ -16,11 +18,20 @@ object Settings {
   case object Normal extends DifficultyLevel(2)
   case object Hard extends DifficultyLevel(3)
 
+  object Time {
+    case class TimeSettings(frameRate: Double = frameRate, timeRatio: Double = timeRatio)
+    val frameRate: Double = 60.0
+    val timeRatio: Double = 1.0
+    val truncate: Double => Double = n => (n * 1000).round / 1000.toDouble
+    val delay: Double => Double = n => truncate(1.0 / n)
+
+    def elapsedTime(frameRate: Double)(implicit timeRatio: Double = 1.0): Double =
+      delay(frameRate) * timeRatio
+  }
+
   /**
    * Wrapper for all settings values. The controller should have an instance of this class and
    * should pass it to the model when is created.
    */
-  case class Settings(difficulty: Difficulty = Normal) {
-    def changeDifficulty(d: Difficulty): Settings = Settings(d)
-  }
+  case class Settings(difficulty: Difficulty = Normal, timeSettings: TimeSettings = TimeSettings())
 }

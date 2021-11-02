69ae438cf95f8382a0b602375443fc553405eb94
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-11-02 15:29:46 +0000

    Format with scalafmt

--- src/main/scala/controller/interaction/GameLoop.scala
+++ src/main/scala/controller/interaction/GameLoop.scala
@@ -42,12 +42,11 @@ object GameLoop {
         model: ActorRef[Update],
         view: ActorRef[Render],
         timeSettings: TimeSettings): Behavior[Input] = Behaviors.setup { ctx =>
-      Behaviors.receiveMessagePartial {
-        case Start() =>
-          Behaviors.withTimers { timers =>
-            timers.startTimerWithFixedDelay(Tick, delay(timeSettings.frameRate).seconds)
-            GameLoopActor(ctx, model, view, timeSettings).running()
-          }
+      Behaviors.receiveMessagePartial { case Start() =>
+        Behaviors.withTimers { timers =>
+          timers.startTimerWithFixedDelay(Tick, delay(timeSettings.frameRate).seconds)
+          GameLoopActor(ctx, model, view, timeSettings).running()
+        }
       }
     }
   }
@@ -103,10 +102,9 @@ object GameLoop {
      * The model and the view are not updated anymore, the [[GameLoop]] is waiting for a message to
      * resume the game.
      */
-    private def paused(): Behavior[Input] = Behaviors.receiveMessagePartial {
-      case ResumeGame() =>
-        model ! ResumeGame()
-        running()
+    private def paused(): Behavior[Input] = Behaviors.receiveMessagePartial { case ResumeGame() =>
+      model ! ResumeGame()
+      running()
     }
   }
 }

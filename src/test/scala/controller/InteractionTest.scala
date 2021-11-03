1d435cfdc285b6ba82fdb8e7214cfe59c445a14d
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-11-03 15:51:10 +0100

    interaction test: add future retrieving to interaction with the model suite

--- src/test/scala/controller/InteractionTest.scala
+++ src/test/scala/controller/InteractionTest.scala
@@ -4,11 +4,13 @@ import akka.actor.testkit.typed.scaladsl.{ ScalaTestWithActorTestKit, TestProbe 
 import akka.actor.typed.scaladsl.AskPattern.{ schedulerFromActorSystem, Askable }
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ ActorRef, Behavior }
+import commons.Futures.retrieve
 import controller.Controller.ControllerActor
-import controller.Controller.ControllerMessages.ActorInteraction
+import controller.Controller.ControllerMessages.{ ActorInteraction, CurrentWallet }
 import controller.interaction.Messages.{ Input, Render, Update, WithReplyTo }
 import controller.settings.Settings.Settings
 import model.managers.GameDynamicsMessages.WalletQuantity
+import model.stats.Stats
 import org.scalatest.wordspec.AnyWordSpecLike
 
 import java.util.concurrent.TimeUnit
@@ -38,7 +40,10 @@ class InteractionTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
     }
     "interacting" should {
       "interact with the model" in {
-        controller ? (ref => ActorInteraction(ref, WalletQuantity(controller)))
+        retrieve(controller ? (ref => ActorInteraction(ref, WalletQuantity(controller)))) {
+          case msg => msg shouldBe CurrentWallet(Stats.startingWallet)
+          case _   => fail()
+        }
         model expectMessage WithReplyTo(WalletQuantity(controller), controller)
       }
     }

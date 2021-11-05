a3548f7733fc06745373f941d106d4a1ff6c54fd
Tommaso Mandoloni <tommaso.mandoloni@studio.unibo.it>
2021-11-05 18:03:55 +0100

    Try to fix TrackLoader

--- src/test/scala/controller/TrackLoaderTest.scala
+++ src/test/scala/controller/TrackLoaderTest.scala
@@ -1,9 +1,10 @@
 package controller
 
-import akka.actor.testkit.typed.scaladsl.{ScalaTestWithActorTestKit, TestProbe}
+import akka.actor.testkit.typed.scaladsl.{ ScalaTestWithActorTestKit, TestProbe }
 import akka.actor.typed.ActorRef
-import akka.actor.typed.scaladsl.AskPattern.{Askable, schedulerFromActorSystem}
+import akka.actor.typed.scaladsl.AskPattern.{ schedulerFromActorSystem, Askable }
 import commons.Futures.retrieve
+import controller.inout.FileCoders.CoderBuilder.appDir
 import controller.inout.TrackLoader.TrackLoaderActor
 import controller.inout.TrackLoader.TrackLoaderMessages._
 import controller.interaction.Messages.Input
@@ -12,10 +13,14 @@ import org.scalatest.wordspec.AnyWordSpecLike
 import view.View.ViewMessages.TrackSaved
 
 import scala.language.postfixOps
+import scala.reflect.io.Path
 
 object TrackLoaderTest {}
 
 class TrackLoaderTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+
+  Path(appDir).deleteRecursively() // Clean directories tree to setup testing environment
+
   val trackLoader: ActorRef[Input] = testKit.spawn(TrackLoaderActor())
   val controllerTest: TestProbe[Input] = testKit.createTestProbe[Input]()
 

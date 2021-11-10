93b0db07718568a3e23effb2347c9d7913b6336d
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-11-10 17:02:38 +0100

    Remove unused code

--- src/main/scala/model/entities/balloons/balloontypes/LeadBalloons.scala
+++ src/main/scala/model/entities/balloons/balloontypes/LeadBalloons.scala
@@ -5,21 +5,14 @@ import model.entities.balloons.balloontypes.BalloonDecorations.BalloonDecoration
 import model.entities.bullets.Bullets.{ Bullet, CannonBall }
 
 object LeadBalloons {
-
   /**
-   * Adds to a [[Balloon]] the ability to being popped only by [[CannonBall]] s.
-   */
-  trait Lead extends Balloon { balloon: Balloon => }
-
-  /**
-   * Wraps a [[Balloon]] with the [[Lead]]
+   * Wraps a [[Balloon]] with the [[model.entities.balloons.BalloonDecorations.Lead]]
    * [[model.entities.balloons.BalloonDecorations.BalloonType]].
    * @param balloon
    *   The [[Balloon]] wrapped.
    */
   case class LeadBalloon(override val balloon: Balloon)
-      extends BalloonDecoration(balloon)
-      with Lead {
+      extends BalloonDecoration(balloon) {
     override def instance(balloon: Balloon): BalloonDecoration = lead(balloon)
 
     override def pop(bullet: Bullet): Option[BalloonDecoration] = bullet match {

a23660f1231787fc763c7a584e38c756c967b90c
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-11-10 17:07:08 +0100

    Performances: update performances values

--- src/test/scala/controller/PerformancesTest.scala
+++ src/test/scala/controller/PerformancesTest.scala
@@ -1,23 +1,20 @@
 package controller
 
-import akka.actor.testkit.typed.scaladsl.{ ScalaTestWithActorTestKit, TestProbe }
-import akka.actor.typed.scaladsl.AskPattern.{ schedulerFromActorSystem, Askable }
-import akka.actor.typed.scaladsl.{ ActorContext, Behaviors }
-import akka.actor.typed.{ ActorRef, Behavior }
+import akka.actor.testkit.typed.scaladsl.{ScalaTestWithActorTestKit, TestProbe}
+import akka.actor.typed.scaladsl.AskPattern.{Askable, schedulerFromActorSystem}
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.actor.typed.{ActorRef, Behavior}
 import commons.Futures.retrieve
 import controller.Controller.ControllerActor
-import controller.Controller.ControllerMessages.{ NewTrack, StartNextRound }
+import controller.Controller.ControllerMessages.{NewTrack, StartNextRound}
 import controller.PerformancesTest.PerformanceMessages._
 import controller.PerformancesTest._
 import controller.interaction.GameLoop.GameLoopMessages._
-import controller.interaction.Messages.{ Input, Render, Update }
+import controller.interaction.Messages.{Input, Render, Update}
 import controller.settings.Settings.Settings
-import controller.settings.Settings.Time.{ delay, elapsedTime, TimeSettings }
+import controller.settings.Settings.Time.{TimeSettings, delay, elapsedTime}
+import model.Model.ModelActor
 import model.Model.ModelMessages.TickUpdate
-import model.actors.BalloonActor
-import model.entities.Entities.Entity
-import model.entities.balloons.Balloons.{ simple, Balloon }
-import model.managers.EntitiesMessages.{ EntityUpdated, ExitedBalloon, UpdateEntity }
 import model.maps.Tracks.Track
 import org.scalatest.Ignore
 import org.scalatest.wordspec.AnyWordSpecLike
@@ -37,46 +34,6 @@ object PerformancesTest {
     case class AveragePerformances(updateAverageTime: Double, updateFrequency: Double)
         extends Render
   }
-
-  /** Dummy model waiting for tick message from game loop. */
-  val runningModel: (List[Entity], List[ActorRef[Update]], ActorRef[Input]) => Behavior[Update] =
-    (entities, actors, stopper) =>
-      Behaviors.setup { ctx =>
-        Behaviors.receiveMessagePartial {
-          case TickUpdate(elapsedTime, replyTo) =>
-            actors foreach {
-              _ ! UpdateEntity(elapsedTime, entities, ctx.self)
-            }
-            updatingModel(entities, actors, replyTo, stopper)
-          case _ => Behaviors.same
-        }
-      }
-
-  /** Dummy model waiting for all entities to be updated. */
-  val updatingModel: (
-      List[Entity],
-      List[ActorRef[Update]],
-      ActorRef[Input],
-      ActorRef[Input]) => Behavior[Update] =
-    (entities, actors, gameLoop, stopper) => {
-      var updatedEntities: List[Entity] = List()
-      Behaviors.receiveMessagePartial {
-        case EntityUpdated(entity, _) =>
-          updatedEntities = entity :: updatedEntities
-          updatedEntities match {
-            case _ if updatedEntities.size == entities.size =>
-              gameLoop ! ModelUpdated(updatedEntities, List())
-              runningModel(updatedEntities, actors, stopper)
-            case _ => Behaviors.same
-          }
-
-        case ExitedBalloon(_, _) =>
-          stopper ! StopPerformances()
-          Behaviors.stopped
-
-        case _ => Behaviors.same
-      }
-    }
 
   /**
    * Dummy game loop that simply ticks the model and takes the time.
@@ -173,15 +130,9 @@ class PerformancesTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
   val performancesStopper: TestProbe[Input] = testKit.createTestProbe[Input]()
   val track: Track = Track()
 
-  val numberOfActors: Int = 10
+  val numberOfRounds: Int = 1
 
-  val entities: List[Balloon] =
-    LazyList.continually(simple() on track in track.start).take(numberOfActors).toList
-
-  val actors: List[ActorRef[Update]] = entities.map(b => testKit spawn BalloonActor(b))
-
-  val model: ActorRef[Update] =
-    testKit spawn runningModel(entities, actors, performancesStopper.ref)
+  val model: ActorRef[Update] = testKit spawn ModelActor(Settings())
 
   val performancesWatcher: ActorRef[Input] = testKit spawn {
     Behaviors.setup { ctx: ActorContext[Input] =>
@@ -205,7 +156,7 @@ class PerformancesTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       "not be slow" in {
         controller ! NewTrack()
         gameLoop ! Start()
-        controller ! StartNextRound()
+        for (_ <- 0 until numberOfRounds) controller ! StartNextRound()
 
         performancesStopper.within(100.seconds) {
           performancesStopper expectMessage StopPerformances()
@@ -213,6 +164,7 @@ class PerformancesTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
             case AveragePerformances(updateAverageTime, renderAverageTime) =>
               println("Render Average Time: " + renderAverageTime + " milliseconds.")
               println("Update Round Trip Average Time: " + updateAverageTime + " milliseconds.")
+              testKit.shutdownTestKit()
             case _ =>
           }
         }
